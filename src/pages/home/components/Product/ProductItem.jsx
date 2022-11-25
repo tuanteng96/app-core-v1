4c54061ebe5e37d3311534f5583734415ee5d76b
tuanteng96 <tuantengyn@gmail.com>
2022-11-25 22:18:26 +0700

    update

--- src/pages/home/components/Product/ProductItem.jsx
+++ src/pages/home/components/Product/ProductItem.jsx
@@ -1,35 +1,40 @@
-import React from "react";
-import { SERVER_APP } from "../../../../constants/config";
-import { checkSale, formatPriceVietnamese } from "../../../../constants/format";
-import { Link } from "framework7-react";
-import RenderTagsProd from "../../../shop/components/RenderTagsProd";
+import React from 'react'
+import { SERVER_APP } from '../../../../constants/config'
+import { checkSale, formatPriceVietnamese } from '../../../../constants/format'
+import { Link } from 'framework7-react'
+import RenderTagsProd from '../../../shop/components/RenderTagsProd'
+import { TruncateLines } from 'react-truncate-lines'
 
 export default class ProductItem extends React.Component {
   constructor() {
-    super();
-    this.state = {};
+    super()
+    this.state = {}
   }
 
   render() {
-    const { item, source } = this.props;
+    const { item, source } = this.props
     return (
-      <Link href={"/shop/detail/" + item.id} className="page-shop__list-item">
+      <Link href={'/shop/detail/' + item.id} className="page-shop__list-item">
         <div className="page-shop__list-img">
           <RenderTagsProd status={item?.source?.Status} />
           <img
-            src={SERVER_APP + "/Upload/image/" + item.photo}
+            src={SERVER_APP + '/Upload/image/' + item.photo}
             alt={item.title}
           />
         </div>
         <div className="page-shop__list-text">
-          <h3>{item.title}</h3>
+          <h3 className="w-100">
+            <TruncateLines lines={2} ellipsis={<span>...</span>}>
+              {item.title}
+            </TruncateLines>
+          </h3>
           <div
             className={
-              "page-shop__list-price " +
+              'page-shop__list-price ' +
               (checkSale(source.SaleBegin, source.SaleEnd, item.pricesale) ===
               true
-                ? "sale"
-                : "")
+                ? 'sale'
+                : '')
             }
           >
             <span className="price">
@@ -43,6 +48,6 @@ export default class ProductItem extends React.Component {
           </div>
         </div>
       </Link>
-    );
+    )
   }
 }
