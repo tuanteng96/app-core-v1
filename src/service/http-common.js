323c21834314acb5cbea06c096beb0f6826b3ca8
tuanteng96 <tuantengyn@gmail.com>
2023-08-18 17:27:36 +0700

    update

--- src/service/http-common.js
+++ src/service/http-common.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { SERVER_APP } from './../constants/config';
-export default axios.create({
-    baseURL: SERVER_APP,
-    headers: {
-        "Content-type": "application/x-www-form-urlencoded"
-    },
-});
+export default typeof ClientZ !== "undefined"
+  ? ClientZ
+  : axios.create({
+      baseURL: SERVER_APP,
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
